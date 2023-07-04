8b9a30d6ef6fe74c40d35da9ebdcafebb50ceccd
Kaushal Dave <kaushal.atul.dave@gmail.com>
2023-07-04 12:20:57 +0200

    [main] clean up
    Removed unused lines of codes

--- source/main.m
+++ source/main.m
@@ -31,12 +31,6 @@ currentFolder = pwd;
 subdirectory = 'functions';  % Replace with your subdirectory name
 subdirectoryPath = fullfile(currentFolder, subdirectory);
 addpath(subdirectoryPath);
-
-currentFolder = pwd;
-subdirectory = 'utils';  % Replace with your subdirectory name
-subdirectoryPath = fullfile(currentFolder, subdirectory);
-addpath(subdirectoryPath);
-
 %% Read input file
 coupling_vars = read_input();
 coupling_vars = evaluate();
